c5069d737b712eba47061c549f87a9a5fae89df6
Puppe1eer <mopsqwertz@web.de>
2022-01-21 13:56:39 +0100

    4.5.1
    small changes in skin..

--- output/skin.js
+++ output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 4.5.3/10717
 // Filename: Rundgang.ggsk
-// Generated Mi 19. Jan 17:15:28 2022
+// Generated Do 20. Jan 09:20:51 2022
 
 function pano2vrSkin(player,base) {
 	var me=this;
